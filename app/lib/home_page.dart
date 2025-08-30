203024e01385095531f0617f5932d4a6b5384c64
Krishna <115911291+Confusedaff@users.noreply.github.com>
2025-08-30 20:38:53 +0530

    centre

--- app/lib/home_page.dart
+++ app/lib/home_page.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Baby is a FUNDA',style: TextStyle(fontWeight: FontWeight.bold),),),
+      appBar: AppBar(title: Center(child: Text('Baby is a FUNDA',style: TextStyle(fontWeight: FontWeight.bold),)),),
       body: Center(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
